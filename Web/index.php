7a451d8f6675f66af16e40d0f02f4d801d3e0246
Mery Téo <166821930+arbr0x@users.noreply.github.com>
2024-09-17 11:58:50 +0200

    Add files via upload

---
+++ Web/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>connexion</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <div class="conteneur">
+        <form action="POST">
+            <div class="image">
+                <img src="./media/profile" alt="">
+            </div>
+            <h1>Connexion</h1>
+            <input type="text" class="identifiant" id="identifiant" name="identifiant" placeholder="identifiant"> <br>
+            <input type="password" class="mdp" id="mdp" name="mdp" placeholder="mot de passe"><br>
+            <a href="#">mot de passe oublié</a><br>
+            <button name="connexion" id="connexion">Connexion</button> <br>
+
+            <?php
+                
+            ?>
+        </form>
+    </div>
+    
+    <footer>
+
+    </footer>
+   
+    
+</body>
+</html>
